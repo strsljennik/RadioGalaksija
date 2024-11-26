34791e1e629d2db1c7101e3a100119cdea8b461c
strsljennik <strsljennik@gmail.com>
2024-11-26 14:58:53 +0100

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 
 // URL tvog servisa na Renderu
-const url = 'https://radiogalaksija.onrender.com/';  // Tvoj URL za servis
+const url = 'https://radiogalaksija-h6c7.onrender.com/';
 
 // Funkcija koja šalje zahtev ka servisu
 const pingService = async () => {
   try {
     const response = await axios.get(url);
-    console.log(`Ping response: ${response.status}`);  // Ispisuje status odgovora (200 OK)
+    console.log(`Ping response: ${response.status}`); // Ispisuje status odgovora (200 OK)
   } catch (error) {
-    console.error('Error pinging service:', error.message);  // Ispisuje grešku ako nešto krene po zlu
+    console.error('Error pinging service:', error.message); // Ispisuje grešku ako nešto krene po zlu
   }
 };
 
-// Pokreće pingovanje svakih 10 minuta (600000 milisekundi)
-setInterval(pingService, 600000);  // 10 minuta
+// Pokreće pingovanje svakih 5 minuta (300000 milisekundi)
+setInterval(pingService, 300000); // 5 minuta
