ae4176ceb228cd1cc6d4f59beb492193347a1a03
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-05-01 18:22:27 +0200

    Create default.js

---
+++ default.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+// Schema za default korisnike
+const defaultUserSchema = new mongoose.Schema({
+    username: String,
+    socketId: String,
+    createdAt: { type: Date, default: Date.now }
+});
+
+// Model
+const DefaultUser = mongoose.model('DefaultUser', defaultUserSchema, 'default');
+
+function setupDefaultUsers(io, guests) {
+    io.on('connection', async (socket) => {
+        // Generiši username
+        const uniqueNumber = generateUniqueNumber();
+        const nickname = `Gost-${uniqueNumber}`;
+        guests[socket.id] = nickname;
+
+        // Emituj nickname
+        socket.emit('setNickname', nickname);
+        socket.emit('yourNickname', nickname);
+
+        // Upisi u kolekciju 'default'
+        const user = new DefaultUser({ username: nickname, socketId: socket.id });
+        await user.save();
+        console.log(`Upisan ${nickname} u kolekciju default.`);
+
+        // Emituj ostalima
+        socket.broadcast.emit('newGuest', nickname);
+        io.emit('updateGuestList', Object.values(guests));
+
+        // Diskonekt
+        socket.on('disconnect', async () => {
+            await DefaultUser.deleteOne({ socketId: socket.id });
+            console.log(`Obrisan ${nickname} iz kolekcije default.`);
+            delete guests[socket.id];
+            io.emit('updateGuestList', Object.values(guests));
+        });
+
+        function generateUniqueNumber() {
+            return Math.floor(Math.random() * 8889) + 1111;
+        }
+    });
+}
+
+module.exports = { setupDefaultUsers };
