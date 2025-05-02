4cbf9a67bc275cb94dc9675713c263a6165ea666
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-05-02 10:46:30 +0200

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -1,24 +1,9 @@
-let myNickname = '';
+let myNickname = ''; // biće postavljen od servera
 
 socket.on('yourNickname', function(nick) {
-        myNickname = nick;
-      startPing();
-    });
+    myNickname = nick;
+});
 
-    function startPing() {
-        setInterval(() => {
-            fetch('/ping', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username: myNickname })
-            })
-            .then(res => {
-                if (!res.ok) console.error('Ping neuspešan');
-                else console.log('Ping poslat za', myNickname);
-            })
-            .catch(err => console.error('Greška pri ping:', err));
-        }, 2 * 60 * 1000); // 2 minuta
-    }
 
 let isBold = false;
 let isItalic = false;
