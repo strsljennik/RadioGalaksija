4145bf9080d10180fd8ae211e05122f688171d06
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-05-24 03:55:20 +0200

    Update strim.js

--- public/strim.js
+++ public/strim.js
@@ -1,3 +1,35 @@
+// ZENO PLAYER NA DUGME
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById('radioStream');
+    var button = document.getElementById('sound');
+    var isPlaying = false;
+
+    button.addEventListener('click', function() {
+          button.blur();
+        if (isPlaying) {
+            audio.pause();
+              button.textContent = "Play";
+          isPlaying = false;
+        } else {
+            playStream();
+        }
+    });
+
+    function playStream() {
+        audio.src = "https://stream.zeno.fm/krdfduyswxhtv";  
+        audio.load();  
+        audio.play().then(() => {
+            button.textContent = "Stop";
+          isPlaying = true;
+        }).catch(error => console.error("Greška pri puštanju zvuka:", error));
+    }
+
+    // Automatsko ponovno pokretanje pri gubitku konekcije
+    audio.addEventListener('error', function() {
+        setTimeout(playStream, 3000);
+    });
+});
+
 let iframeVisible = false;
 let iframeClicked = false;
 
