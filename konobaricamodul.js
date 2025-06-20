ab87eedc9029dc10d3ecb1c3c92b09f5f9604e3b
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-06-20 07:42:13 +0200

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -25,7 +25,7 @@ const Guest = mongoose.model('Guest', GuestSchema);
     socket.emit('initialChatContainerData', chatContainerState);
   });
 socket.on('new_guest', () => {
-  const greetingMessage = `Dobro došli, osećajte se kao kod kuće. Vase muzicke zelje potrudice se da vam ispune Dj LiLi, Dj Sandra i moja malenkost, amater Dj *__X__*. 
+  const greetingMessage = `Dobro došli, osećajte se kao kod kuće. Vase muzicke zelje potrudice se da vam ispune Dj LiLi, Dj Sandra i amater Dj *__X__*. 
   Kada nismo na smeni tu je nama uvek nas dragi Auto Dj.  Uzivajte
   <img src="emoji gif/luster.webp" alt="emoji">`;
   io.emit('message', {
