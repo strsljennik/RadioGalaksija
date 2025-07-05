ea87bebfaec63c283b80a2910f83d9ce7abaf5df
strsljennik <strsljennik@gmail.com>
2025-07-05 15:10:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava');
 const { setupSocketEvents } = require('./banmodul'); // Uvoz funkcije iz banmodula
 const konobaricaModul = require('./konobaricamodul'); // Uvoz konobaricamodul.js
+const slikemodul = require('./slikemodul');
 const pingService = require('./ping');
 const privatmodul = require('./privatmodul'); // Podesi putanju ako je u drugom folderu
 require('dotenv').config();
@@ -24,6 +25,7 @@ const io = socketIo(server, {
 
 connectDB(); // Povezivanje na bazu podataka
 konobaricaModul(io);
+slikemodul.setSocket(io);
 
 // Middleware za parsiranje JSON podataka i serviranje statičkih fajlova
 app.use(express.json());
